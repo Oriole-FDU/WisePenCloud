7f5039d568f5db706e9e5d43819feb1fc57f5432
NeoSunJZ <neosunjz@qq.com>
2026-02-21 10:58:58 +0800

    refactor(Auth): 重构了登录鉴权模块
    废弃 JWT 无状态方案。不再将用户信息打包在 JWT 载荷（Payload）中，改为将会话状态与用户详细信息持久化集中存储至 MongoDB。
    为弥补状态化带来的性能损耗，设计并实现了 APISIX 本地内存（L1 缓存 session_cache）与后端 Redis（L2 缓存）的双级缓存机制，保障鉴权链路的及时响应。
    移除 Sa-Token 框架依赖。自行实现了一套轻量级、高度定制化的安全校验注解（@CheckLogin、@CheckRole），将接口鉴权规则的定义权完全收归微服务侧。
    重构 APISIX Lua 脚本并应用全局统一模板。网关层不再负责强制拦截未登录请求，而是将解析到的会话状态完整透传，由后端服务根据自定义注解进行精准拦截。
    重构了微服务内部的上下文 ContextHolder，将原有的“用户组 ID 列表”数据结构升级为“带有具体用户组权限的 Map”，为后续复杂的细粒度权限校验提供完整的数据支撑。

--- wisepen-common/src/main/java/com/oriole/wisepen/common/core/domain/enums/ResultCode.java
+++ wisepen-common/src/main/java/com/oriole/wisepen/common/core/domain/enums/ResultCode.java
@@ -13,16 +13,8 @@ public enum ResultCode implements IErrorCode {
 
     // 系统级
     SYSTEM_ERROR(500, "系统内部错误"),
-    PARAM_ERROR(400, "参数错误"),
+    PARAM_ERROR(400, "参数错误");
 
-    // --- 认证/权限 (严格对应 Sa-Token 异常) ---
-    NOT_LOGIN(401, "账号未登录"),             // 对应 NotLoginException
-    NO_PERMISSION(403, "没有权限访问该资源"),   // 对应 NotPermissionException
-    NO_ROLE(403, "没有角色权限访问该资源");     // 对应 NotRoleException
-
-
-    // 业务模块 (2000-2999)
-    // ..暂无
 
     private final Integer code;
     private final String msg;
--- wisepen-user-service/wisepen-user-biz/src/main/java/com/oriole/wisepen/user/service/GroupService.java
+++ wisepen-user-service/wisepen-user-biz/src/main/java/com/oriole/wisepen/user/service/GroupService.java
@@ -3,13 +3,18 @@ package com.oriole.wisepen.user.service;
 import com.oriole.wisepen.user.domain.entity.Group;
 
 import java.util.List;
+import java.util.Map;
+
 public interface GroupService {
-    // 创建分组 (业务方法)
+    // 创建分组
     void createGroup(Group group);
 
-    // 获取用户的所有组ID (业务方法)
+    // 获取用户的所有组ID和角色
+    Map<String, Integer> getGroupRoleMapByUserId(Long userId);
+
+    // 获取用户的所有组ID
     List<Long> getGroupIdsByUserId(Long userId);
 
-    // 获取组的详情 (业务方法)
+    // 获取组的详情
     Group getGroupById(Long id);
 }
