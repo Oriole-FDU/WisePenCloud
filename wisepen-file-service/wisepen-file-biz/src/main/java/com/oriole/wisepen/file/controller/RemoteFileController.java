8f4da9a28f1bfd90aa3149cb4e0e8fe759b25f95
AUSIO2 <ian666xiong@gmail.com>
2026-03-02 22:10:13 +0800

    refractor(file-service):移除sa-token依赖

---
+++ wisepen-file-service/wisepen-file-biz/src/main/java/com/oriole/wisepen/file/controller/RemoteFileController.java
@@ -0,0 +1,39 @@
+package com.oriole.wisepen.file.controller;
+
+import com.oriole.wisepen.common.core.domain.R;
+import com.oriole.wisepen.file.api.domain.dto.FileInfoVO;
+import com.oriole.wisepen.file.domain.entity.FileInfo;
+import com.oriole.wisepen.file.mapper.FileMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remote/file")
+@RequiredArgsConstructor
+public class RemoteFileController {
+
+    private final FileMapper fileMapper;
+
+    @GetMapping("/info/{fileId}")
+    public R<FileInfoVO> getFileInfo(@PathVariable("fileId") Long fileId) {
+        FileInfo fileInfo = fileMapper.selectById(fileId);
+        if (fileInfo == null) {
+            return R.fail("文件不存在");
+        }
+        return R.ok(toFileInfoVO(fileInfo));
+    }
+
+    private FileInfoVO toFileInfoVO(FileInfo fileInfo) {
+        return FileInfoVO.builder()
+                .documentId(fileInfo.getId())
+                .fileName(fileInfo.getFilename())
+                .fileSize(fileInfo.getSize())
+                .createTime(fileInfo.getCreateTime())
+                .status(fileInfo.getStatus())
+                .createBy(fileInfo.getCreateBy())
+                .build();
+    }
+}
