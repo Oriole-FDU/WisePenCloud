2ddbe5ebe3694fc8319ddd3efebef7a03bd80b72
AUSIO2 <ian666xiong@gmail.com>
2026-03-05 13:30:59 +0800

    feat(file-service):实现文件上传成功后注册为资源

--- wisepen-file-service/wisepen-file-biz/src/main/java/com/oriole/wisepen/file/FileApplication.java
+++ wisepen-file-service/wisepen-file-biz/src/main/java/com/oriole/wisepen/file/FileApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 文件服务启动类
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.oriole.wisepen.file.mapper")
+@EnableFeignClients(basePackages = "com.oriole.wisepen.resource.feign")
 public class FileApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class, args);
