cebc95adb0c62b4a8d08b6d4c928ab8b605caf69
AUSIO2 <ian666xiong@gmail.com>
2026-03-24 23:29:37 +0800

    feat(user-service):注册时设置nickname为username

--- wisepen-user-service/wisepen-user-biz/src/main/java/com/oriole/wisepen/user/service/impl/UserServiceImpl.java
+++ wisepen-user-service/wisepen-user-biz/src/main/java/com/oriole/wisepen/user/service/impl/UserServiceImpl.java
@@ -107,6 +107,7 @@ public class UserServiceImpl implements UserService {
         // 新建未验证的学生用户
         UserEntity user = UserEntity.builder()
                 .username(req.getUsername())
+                .nickname(req.getUsername())
                 .identityType(IdentityType.STUDENT)
                 .status(Status.UNIDENTIFIED)
                 .build();
